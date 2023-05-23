71c2085c86abab568063a2dca4908375adcdb946
JeanGautierSherpas <jean@sherpas.com>
2023-05-23 12:58:15 +0200

    add type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CocktailModule } from './cocktail/cocktail.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const { env } = process;
+const isTestEnv = env.NODE_ENV === 'test';
+
+const typeOrmModuleOptions: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: env.DB_HOST,
+  port: parseInt(env.DB_PORT ?? '3306'),
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: isTestEnv ? 'test' : env.DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  subscribers: ['./subscribers/*.ts'],
+  synchronize: false,
+  charset: 'utf8mb4',
+  migrationsRun: true,
+  migrations: ['./migration/*.ts'],
+  extra: {
+    decimalNumbers: true,
+  },
+  timezone: 'Z',
+  logging: false,
+  autoLoadEntities: true,
+  retryAttempts: 0,
+};
 
 @Module({
-  imports: [CocktailModule, UserModule],
+  imports: [
+    CocktailModule,
+    UserModule,
+    TypeOrmModule.forRoot(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
